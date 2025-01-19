02c08d8dc73a5ea9f30a33a8da242fb15bc83d12
myogg <xxjss@163.com>
2025-01-19 10:25:29 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -13,8 +13,9 @@ My personal blog using issues and GitHub Actions (参考[yihong](https://github.
 
 
 * 用文字记录我的胡思乱想与生活的瞬间，我疯狂的想法与可能为之的行动。  
+* 一个从[农学研究生退学](https://github.com/QiYongchuan/MyGitBlog/issues/84)转行到计算机行业的年轻人。
 * 记录在这个时代下的焦虑、迷茫、挣扎与希望。
-* [About me](https://github.com/myogg/myogg)
+* [About me](https://github.com/QiYongchuan/Qiyongchuan)
 
 ### 提醒自己：
 #### 1.放弃向他人证明自己，放弃向自己证明自己。专注忘我地去做你应该做的事情，心无旁骛地去解决问题。当你脚踏实地的走自己的路时，那种拼命想要证明什么的冲动就会越来越少。你也会因此变得轻松、自由。
@@ -37,7 +38,7 @@ FRIENDS_TABLE_HEAD = "| Name | Link | Desc | \n | ---- | ---- | ---- |\n"
 FRIENDS_TABLE_TEMPLATE = "| {name} | {link} | {desc} |\n"
 FRIENDS_INFO_DICT = {
     "名字": "hhh",
-    "链接": "www.bing.com",
+    "链接": "www.baidu.com",
     "描述": "test",
 }
 
